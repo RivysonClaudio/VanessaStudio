2c2e46d13343d9bc274a72a8ebf9a5cbedf1fd22
rivyson claudio <orivyson@gmail.com>
2024-12-31 11:51:45 -0300

    feat: new modules implamentation (Services + Workers)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,47 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
+import { ModalComponent } from './components/modal/modal.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ModalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 export class AppComponent {
-  title = 'vanessa-studio-angular';
+  title: string = 'vanessa-studio-angular';
+  showNotification: boolean = false;
+  notificationMessage: string = "";
+  isModalOpen = false;
+  modalData = {title: '', message: ''};
+  private callback?: () => void;
 
-  static formatPhoneNumber(number: string): string{
-    let numberFormated: string = "";
+  notify(message: string, time: number): void{
+    this.notificationMessage = message;
+    this.showNotification = true;
+    setTimeout(() => {
+      this.showNotification = false;
+      this.notificationMessage = "";
+    }, time);
+  }
 
-    numberFormated = number.replace(/(\d{2})(\d{4,5})(\d{4})/, '($1) $2-$3');
+  desableNotification(): void{
+    this.showNotification = false;
+  }
 
-    return numberFormated;
+  confirmModal(): void{
+    if(this.callback){this.callback()}
+    this.closeModal();
+  }
+
+  openModal(data: {title: string, message: string}, callback: () => void) {
+    this.modalData = data;
+    this.callback = callback;
+    this.isModalOpen = true;
+  }
+
+  closeModal() {
+    this.isModalOpen = false;
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,9 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ClientesPanelComponent } from './components/clientes/clientes-panel/clientes-panel.component';
 import { ClientesFormComponent } from './components/clientes/clientes-form/clientes-form.component';
+import { SeviceListComponent } from './components/services/sevice-list/sevice-list.component';
+import { SeviceFormComponent } from './components/services/sevice-form/sevice-form.component';
+import { WorkerPanelComponent } from './components/workers/worker-panel/worker-panel.component';
 
 export const routes: Routes = [
     {
@@ -22,5 +25,23 @@ export const routes: Routes = [
         path: "clientes/:id",
         component: ClientesFormComponent,
         data: {type: 'edit'}
+    },
+    {
+        path: "servicos",
+        component: SeviceListComponent
+    },
+    {
+        path: "servicos/registrar",
+        component: SeviceFormComponent,
+        data: {type: 'register'}
+    },
+    {
+        path: "servicos/consulta/:id",
+        component: SeviceFormComponent,
+        data: {type: 'edit'}
+    },
+    {
+        path: "profissionais",
+        component: WorkerPanelComponent
     }
 ];
